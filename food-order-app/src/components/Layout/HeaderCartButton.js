27658a790b734db22c3c222807bdcc68551eeb22
Gabriel Renard <gabriel.renard@protonmail.com>
2022-11-10 14:11:55 +0100

    create Modal component with React portal

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -2,9 +2,11 @@ import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = () => {
+
+  
   return (
     <>
-      <button className={classes.button}>
+      <button className={classes.button} onClick={}>
         <span className={classes.icon}>
           <CartIcon />
         </span>
