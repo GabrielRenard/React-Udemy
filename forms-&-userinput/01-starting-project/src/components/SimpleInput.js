ae1f1942711b2b149ab5c1e8db7093fffc4b74cc
Gabriel Renard <gabriel.renard@protonmail.com>
2022-11-16 13:52:46 +0100

    first commit

--- forms-&-userinput/01-starting-project/src/components/SimpleInput.js
+++ forms-&-userinput/01-starting-project/src/components/SimpleInput.js
@@ -1,9 +1,34 @@
-const SimpleInput = (props) => {
+import { useRef, useState } from "react";
+
+const SimpleInput = props => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputHandler = e => {
+    setEnteredName(e.target.value); // set entered name on each keystroke
+  };
+
+  const formSubmitHandler = e => {
+    e.preventDefault();
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value; // set entered name only upon submit
+    console.log(enteredValue);
+
+    // nameInputRef.current.value = ""; => not ideal, leave DOM manipulation to react and not vanilla JS
+    setEnteredName(""); // set state input to an empty string
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputHandler}
+          value={enteredName} //bind the value to set setEnteredName to empty string
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
