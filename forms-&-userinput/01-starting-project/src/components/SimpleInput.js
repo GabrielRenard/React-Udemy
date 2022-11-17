277b9669071bb39a46cebcab8d8c2fc9ecb1a293
Gabriel Renard <gabriel.renard@protonmail.com>
2022-11-17 10:48:54 +0100

    add email input and validation

--- forms-&-userinput/01-starting-project/src/components/SimpleInput.js
+++ forms-&-userinput/01-starting-project/src/components/SimpleInput.js
@@ -4,22 +4,35 @@ const SimpleInput = props => {
   const [enteredName, setEnteredName] = useState("");
   // const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false); // Checks if user had a chance of editing the input
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
 
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+  const enteredEmailIsValid = /\S+@\S+\.\S+/.test(enteredEmail);
+  const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
+
   let formIsValid = false;
 
-  if (enteredNameIsValid) {
+  if (enteredNameIsValid && enteredEmailIsValid) {
     formIsValid = true;
   }
 
   const nameInputHandler = e => {
-    setEnteredName(e.target.value); // set entered name on each keystroke
+    setEnteredName(e.target.value);
   };
 
-  const nameInputBlurHandler = e => {
+  const nameInputBlurHandler = () => {
     setEnteredNameTouched(true);
+  };
+
+  const emailInputHandler = e => {
+    setEnteredEmail(e.target.value);
+  };
+
+  const emailInputBlurHandler = () => {
+    setEnteredEmailTouched(true);
   };
 
   const formSubmitHandler = e => {
@@ -34,13 +47,19 @@ const SimpleInput = props => {
 
     // nameInputRef.current.value = ""; => not ideal, leave DOM manipulation to react and not vanilla JS
     setEnteredName(""); // set state input to an empty string
+    setEnteredEmail(""); // set state input to an empty string
     setEnteredNameTouched(false);
+    setEnteredEmailTouched(false);
   };
 
   /* Name input should be invalid if only both setEnteredNameIsValid is false
   AND enteredNameTouched (user had a chance to edit input field) is true */
 
   const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
+  const emailInputClasses = emailInputIsInvalid
     ? "form-control invalid"
     : "form-control";
 
@@ -56,8 +75,24 @@ const SimpleInput = props => {
           value={enteredName} //bind the value to set setEnteredName to empty string
         />
       </div>
-      {nameInputIsInvalid && (
+      <div className={emailInputClasses}>
+        <label htmlFor="email">Your Email</label>
+        <input
+          type="email"
+          id="email"
+          onChange={emailInputHandler}
+          onBlur={emailInputBlurHandler}
+          value={enteredEmail} //bind the value to set setEnteredName to empty string
+        />
+      </div>
+      {nameInputIsInvalid && !emailInputIsInvalid && (
         <p className="error-text">Name must not be empty</p>
+      )}
+      {!nameInputIsInvalid && emailInputIsInvalid && (
+        <p className="error-text">Email must not be empty</p>
+      )}
+      {nameInputIsInvalid && emailInputIsInvalid && (
+        <p className="error-text">Fields must not be empty</p>
       )}
       <div className="form-actions">
         <button disabled={!formIsValid}>Submit</button>
