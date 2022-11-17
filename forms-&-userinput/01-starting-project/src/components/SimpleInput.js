c00b8180a9841bf78e7ccab0f9a8263224da102e
Gabriel Renard <gabriel.renard@protonmail.com>
2022-11-17 10:17:24 +0100

    add more validation including formIsValid in SimpleInput.JS

--- forms-&-userinput/01-starting-project/src/components/SimpleInput.js
+++ forms-&-userinput/01-starting-project/src/components/SimpleInput.js
@@ -1,26 +1,25 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 const SimpleInput = props => {
-  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  // const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false); // Checks if user had a chance of editing the input
+
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
 
   const nameInputHandler = e => {
     setEnteredName(e.target.value); // set entered name on each keystroke
-    if (e.target.value.trim() !== "") {
-      setEnteredNameIsValid(true);
-    } else {
-      setEnteredNameIsValid(false);
-      setEnteredNameTouched(true);
-    }
   };
 
   const nameInputBlurHandler = e => {
     setEnteredNameTouched(true);
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
-    }
   };
 
   const formSubmitHandler = e => {
@@ -28,24 +27,18 @@ const SimpleInput = props => {
 
     e.preventDefault();
     // If input is empty, do nothing (dont run code below i.e return)
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
+    if (!enteredNameIsValid) {
       return;
     }
-
-    setEnteredNameIsValid(true);
-
     console.log(enteredName);
-    const enteredValue = nameInputRef.current.value; // set entered name only upon submit
-    console.log(enteredValue);
 
     // nameInputRef.current.value = ""; => not ideal, leave DOM manipulation to react and not vanilla JS
     setEnteredName(""); // set state input to an empty string
+    setEnteredNameTouched(false);
   };
 
   /* Name input should be invalid if only both setEnteredNameIsValid is false
   AND enteredNameTouched (user had a chance to edit input field) is true */
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputClasses = nameInputIsInvalid
     ? "form-control invalid"
@@ -56,7 +49,6 @@ const SimpleInput = props => {
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={nameInputRef}
           type="text"
           id="name"
           onChange={nameInputHandler}
@@ -68,7 +60,7 @@ const SimpleInput = props => {
         <p className="error-text">Name must not be empty</p>
       )}
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
